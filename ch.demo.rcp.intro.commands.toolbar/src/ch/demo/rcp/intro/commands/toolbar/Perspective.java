4dfa7be89bea13bb21954d7929e156446692b23f
ckulesza <ckulesza@hsz-t.ch>
2011-09-16 13:32:01 +0200

    Command in der View Toolbar
    Signed-off-by: ckulesza <ckulesza@hsz-t.ch>

--- ch.demo.rcp.intro.commands.toolbar/src/ch/demo/rcp/intro/commands/toolbar/Perspective.java
+++ ch.demo.rcp.intro.commands.toolbar/src/ch/demo/rcp/intro/commands/toolbar/Perspective.java
@@ -6,9 +6,10 @@ import org.eclipse.ui.IPerspectiveFactory;
 public class Perspective implements IPerspectiveFactory {
 
 	public void createInitialLayout(IPageLayout layout) {
+		String editorArea = layout.getEditorArea();
 		layout.setEditorAreaVisible(false);
 		layout.setFixed(true);
-		
+		layout.addView(View.ID, IPageLayout.LEFT, 1.0f, editorArea);
 	}
 
 }
