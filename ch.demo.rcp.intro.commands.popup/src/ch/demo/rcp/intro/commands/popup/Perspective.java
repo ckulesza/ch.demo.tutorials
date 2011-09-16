dae5dae39b4796e4562e70ad7e411e74448d7938
ckulesza <ckulesza@hsz-t.ch>
2011-09-16 13:53:11 +0200

    Command wird in einem Popup Menu aufgerufen, welches einem View zugewiesen ist.
    Signed-off-by: ckulesza <ckulesza@hsz-t.ch>

---
+++ ch.demo.rcp.intro.commands.popup/src/ch/demo/rcp/intro/commands/popup/Perspective.java
@@ -0,0 +1,14 @@
+package ch.demo.rcp.intro.commands.popup;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+		layout.setEditorAreaVisible(false);
+		layout.setFixed(true);
+		
+	}
+
+}
