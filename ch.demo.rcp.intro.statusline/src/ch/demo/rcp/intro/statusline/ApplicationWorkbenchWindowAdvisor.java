e2132ecc2dac16df164f0921f0948f1b0a65765f
ckulesza <ckulesza@hsz-t.ch>
2011-09-16 13:10:39 +0200

    Demoprogramm für die Nutzung der Statusleiste und das speichern des Layouts.
    Signed-off-by: ckulesza <ckulesza@hsz-t.ch>

---
+++ ch.demo.rcp.intro.statusline/src/ch/demo/rcp/intro/statusline/ApplicationWorkbenchWindowAdvisor.java
@@ -0,0 +1,35 @@
+package ch.demo.rcp.intro.statusline;
+
+import org.eclipse.jface.action.IStatusLineManager;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.ui.application.ActionBarAdvisor;
+import org.eclipse.ui.application.IActionBarConfigurer;
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
+
+	public ApplicationWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		super(configurer);
+	}
+
+	public ActionBarAdvisor createActionBarAdvisor(
+			IActionBarConfigurer configurer) {
+		return new ApplicationActionBarAdvisor(configurer);
+	}
+
+	public void preWindowOpen() {
+		IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
+		configurer.setInitialSize(new Point(400, 300));
+		configurer.setShowCoolBar(false);
+		configurer.setShowStatusLine(true);
+	}
+
+	@Override
+	public void postWindowOpen() {
+		IStatusLineManager statusline = getWindowConfigurer()
+				.getActionBarConfigurer().getStatusLineManager();
+		statusline.setMessage(null, "Status line is ready");
+	}
+}
