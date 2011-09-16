e3e2beefd607e79969a6a46472412cfbf8d3190a
ckulesza <ckulesza@hsz-t.ch>
2011-09-16 12:28:46 +0200

    Demoprogramm, mit dem die Applikation in die Tray Leiste minimiert wird.
    Signed-off-by: ckulesza <ckulesza@hsz-t.ch>

---
+++ ch.demo.rcp.intro.traytest/src/ch/demo/rcp/intro/traytest/Perspective.java
@@ -0,0 +1,10 @@
+package ch.demo.rcp.intro.traytest;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+	}
+}
