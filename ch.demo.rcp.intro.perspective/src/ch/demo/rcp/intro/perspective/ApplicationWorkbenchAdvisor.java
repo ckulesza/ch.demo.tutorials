62d7fbe855e4e91523ad296fd5203761446b51b8
ckulesza <ckulesza@hsz-t.ch>
2011-09-16 10:55:04 +0200

    Demo RCP Projekt erstellt, das die Nutzung der Perspektiven zeigt.
    Signed-off-by: ckulesza <ckulesza@hsz-t.ch>

---
+++ ch.demo.rcp.intro.perspective/src/ch/demo/rcp/intro/perspective/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,20 @@
+package ch.demo.rcp.intro.perspective;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "ch.demo.rcp.intro.perspective.perspective";
+
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+
+}
