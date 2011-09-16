a88fa552c00971510b599546f086656108f245b0
ckulesza <ckulesza@hsz-t.ch>
2011-09-16 10:46:04 +0200

    Simples RCP Commandbeispiel. Beendet das Programm über das Menu.
    Signed-off-by: ckulesza <ckulesza@hsz-t.ch>

---
+++ ch.demo.rcp.commands.first/src/ch/demo/rcp/commands/first/Perspective.java
@@ -0,0 +1,10 @@
+package ch.demo.rcp.commands.first;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+	}
+}
